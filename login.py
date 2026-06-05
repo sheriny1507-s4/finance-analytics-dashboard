ed7e30b9ea3b2c694a40dc93755a5f1432a961cc
sherin .y <sheriny1507@gmail.com>
2026-06-05 22:04:00 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,85 +1,21 @@
 import streamlit as st
-import pandas as pd
-import os
-
-USER_FILE = "users.csv"
-
-def create_user_file():
-    if not os.path.exists(USER_FILE):
-        df = pd.DataFrame({
-            "username": ["admin"],
-            "password": ["admin123"]
-        })
-        df.to_csv(USER_FILE, index=False)
 
 def login():
 
-    create_user_file()
+    st.title("🔐 Login")
 
-    st.title("🔐 Login System")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-    menu = st.sidebar.selectbox(
-        "Menu",
-        ["Login", "Register"]
-    )
+    if st.button("Login"):
 
-    users = pd.read_csv(USER_FILE)
+        if username.strip() == "admin" and password.strip() == "admin123":
 
-    if menu == "Login":
+            st.session_state["logged_in"] = True
+            st.session_state["username"] = username
 
-        username = st.text_input("Username")
-        password = st.text_input(
-            "Password",
-            type="password"
-        )
+            st.success("Login Successful")
+            st.rerun()
 
-        if st.button("Login"):
-
-            valid_user = users[
-                (users["username"] == username)
-                &
-                (users["password"] == password)
-            ]
-
-            if len(valid_user) > 0:
-
-                st.session_state["logged_in"] = True
-                st.session_state["username"] = username
-
-                st.success("Login Successful")
-                st.rerun()
-
-            else:
-                st.error("Invalid Username or Password")
-
-    elif menu == "Register":
-
-        new_user = st.text_input("Create Username")
-        new_pass = st.text_input(
-            "Create Password",
-            type="password"
-        )
-
-        if st.button("Register"):
-
-            if new_user in users["username"].values:
-
-                st.warning(
-                    "Username already exists"
-                )
-
-            else:
-
-                users.loc[len(users)] = [
-                    new_user,
-                    new_pass
-                ]
-
-                users.to_csv(
-                    USER_FILE,
-                    index=False
-                )
-
-                st.success(
-                    "Registration Successful"
-                )
+        else:
+            st.error("Invalid Username or Password")
