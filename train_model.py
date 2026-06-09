3bd21cdad514ec438abb5cfe5bbbc9aafc4fc344
sherin .y <sheriny1507@gmail.com>
2026-06-09 21:29:29 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pickle
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# Load dataset
+df = pd.read_csv("rule_based_labeled_dataset.csv")
+
+# Features and labels
+X = df["Description"]
+y = df["Category"]
+
+# Create model pipeline
+model = Pipeline([
+    ("tfidf", TfidfVectorizer()),
+    ("classifier", MultinomialNB())
+])
+
+# Train model
+model.fit(X, y)
+
+# Save model
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("✅ model.pkl created successfully")
